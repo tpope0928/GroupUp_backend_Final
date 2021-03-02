0d6cce8dc94113a56978d5ddbaf802051c15d57e
tpope0928 <tpope0928@gmail.com>
2021-03-01 22:08:52 -0500

    player logic

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,2 +1,44 @@
 class Api::V1::PlayersController < ApplicationController
+    def index
+        @players = Player.all 
+        # .order('name ASC')
+        render json: @players
+    end 
+
+    def show
+        @player = Player.find(params[:id])
+        render json: @player, status: 200
+    end 
+
+    def create
+        @player = Player.create(player_params)
+        render json: @player, status: 200
+    end
+
+
+    def update 
+        @player.update(player_params)
+        if @player.save
+          render json: @player, status: 200
+        else
+          render json: { errors: @player.errors.full_messages }, status: :unprocessible_entity
+        end
+    end 
+
+    def destroy
+        player = Player.find_by(id: params[:id])
+        player.destroy
+        render json: player
+    end
+
+
+    private
+
+    def player_params
+        params.permit(:name, :city, :state)
+    end
+
+    def find_player
+    @player = Player.find(params[:id])
+    end
 end
