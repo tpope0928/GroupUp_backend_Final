1ce84a80f0a84b4ebd49673e93f6331e7f33883d
tpope0928 <tpope0928@gmail.com>
2021-03-01 22:13:53 -0500

    serializers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Player.destroy_all
 Game.destroy_all
+Player.destroy_all
+
 
 
 Player.create(name: 'KingTusk', city: 'Nashville', state: 'Tennesse')
