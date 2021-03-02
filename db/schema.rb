e1a68cfe14d007392bea73cbda28182debcd7008
tpope0928 <tpope0928@gmail.com>
2021-03-01 21:57:59 -0500

    SEEDS and connection made

---
+++ db/schema.rb
@@ -0,0 +1,38 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_03_02_025129) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.string "genre"
+    t.integer "skill_level"
+    t.string "game_name"
+    t.bigint "player_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["player_id"], name: "index_games_on_player_id"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "state"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "games", "players"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Player.destroy_all
+Game.destroy_all
+
+
+Player.create(name: 'KingTusk', city: 'Nashville', state: 'Tennesse')
+Game.create(player_id: 1, title: 'Call of Duty: Warzone', genre: 'BR', skill_level: '4', game_name: 'KingTusk')
+Game.create(player_id: 1, title: 'Valorant', genre: 'FPS', skill_level: '3', game_name: 'TuskKing')
+
+Player.create(name: 'alecT', city: 'Houston', state: 'Texas')
+Game.create(player_id: 2, title: 'Valorant', genre: 'FPS', skill_level: '3', game_name: 'TrumbleInDaJungle')
+Game.create(player_id: 2, title: 'Fall Guys', genre: 'Casual', skill_level: '2', game_name: 'TrumbleTime')
+
+
+Player.create(name: 'drewP', city: 'Gaithersburg', state: 'Maryland')
+Game.create(player_id: 3, title: 'Dota2', genre: 'MOBA', skill_level: '3', game_name: 'Flanders')
